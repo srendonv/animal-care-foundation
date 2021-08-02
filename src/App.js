f5b3eecf96cc1e21ec1b87d104a0b506acbc7489
Santiago Rendon Valencia <santiago.rendon@fau.de>
2021-08-03 01:04:38 +0200

    registro usuario (action-reducer-view)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
+import setAuthToken from "./utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 // import Nav from "react-bootstrap/Nav";
 // import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
@@ -7,8 +10,9 @@ import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
 import "./assets/css/App.css";
 
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import CreateStudent from "./components/CreateStudent/create-student.component";
 import EditStudent from "./components/edit-student.component";
@@ -18,35 +22,71 @@ import servicesComponent from "./components/servicesComponent";
 // import Login from "./components/Login";
 import CustomerCreate from "./components/CustomerCreate";
 import Footer from "./components/pages/Footer/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <Navbar />
-        </header>
-
-        <Container>
-          <Row>
-            <Col md={12}>
-              <div className="wrapper">
-                <Switch>
-                  <Route exact path="/" component={CreateStudent} />
-                  <Route path="/create-student" component={CreateStudent} />
-                  <Route path="/edit-student/:id" component={EditStudent} />
-                  <Route path="/student-list" component={StudentList} />
-                  <Route path="/services" component={servicesComponent} />
-                  <Route path="/customer-create" component={CustomerCreate} />
-                </Switch>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-
-        <Footer />
-      </div>
-    </Router>
-  );
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = "./login";
+  }
 }
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <Navbar />
+            </header>
+            <Container>
+              <Row>
+                <Col md={12}>
+                  <div className="wrapper">
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                    <Switch>
+                      <Route exact path="/" component={CreateStudent} />
+                      <Route path="/create-student" component={CreateStudent} />
+                      <Route path="/edit-student/:id" component={EditStudent} />
+                      <Route path="/student-list" component={StudentList} />
+                      <Route path="/services" component={servicesComponent} />
+                      <Route
+                        path="/customer-create"
+                        component={CustomerCreate}
+                      />
+                      <PrivateRoute
+                        exact
+                        path="/dashboard"
+                        component={Dashboard}
+                      />
+                    </Switch>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
 export default App;
