80963169e123c107f68cca519c54b7912438b15d
Santiago Rendon Valencia <srendonv@unal.edu.co>
2021-08-12 19:56:00 +0200

    navbar con dropmenu

--- src/components/pages/headerPage/navbar/Navbar.js
+++ src/components/pages/headerPage/navbar/Navbar.js
@@ -9,12 +9,15 @@ import { IconContext } from "react-icons/lib";
 import { Link as ScrollLink } from "react-scroll";
 import { HashLink } from "react-router-hash-link";
 import { connect } from "react-redux";
-import {logoutUser} from "../../../../actions/authActions"
+import { logoutUser } from "../../../../actions/authActions";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { LinkContainer } from "react-router-bootstrap";
 
-const Navbar = ({ auth,logoutUser }) => {
+const Navbar = ({ auth, logoutUser }) => {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(auth.isAuthenticated);
+
+  const { user } = auth;
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
@@ -35,159 +38,160 @@ const Navbar = ({ auth,logoutUser }) => {
 
   console.log(auth);
   return (
-    <>
-      Hola {auth.isAuthenticated ? "True" : "False"}
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <nav className="navbar">
-          <input
-            onChange={(e) => {
-              setIsAuthenticated(e.target.value);
-            }}
-          ></input>
-          <div className="navbar-container container">
-            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-              <MdPets className="navbar-icon" />
-              Animal Care
-            </Link>
-            <div className="menu-icon" onClick={handleClick}>
-              {click ? <FaTimesCircle /> : <FaBars />}
-              {isAuthenticated}
-            </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li className="nav-item">
-                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
-                  Inicio
-                </Link>
-              </li>
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <nav className="navbar fixed-top">
+        <div className="navbar-container container ">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <MdPets className="navbar-icon" />
+            Animal Care
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimesCircle /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Inicio
+              </Link>
+            </li>
 
-              <li className="nav-item">
-                <Link
-                  to="/services"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Servicios
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <HashLink
+                to="/#planes"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Planes
+              </HashLink>
+            </li>
+
+            <li className="nav-item">
+              <HashLink
+                to="/#beneficios"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Beneficios
+              </HashLink>
+            </li>
+
+            <li className="nav-item">
+              <HashLink
+                to="/#equipo"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Equipo
+              </HashLink>
+            </li>
+
+            <li className="nav-item">
+              <HashLink
+                to="/#footer"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Encuéntranos
+              </HashLink>
+            </li>
+
+            {button ? (
+              auth.isAuthenticated ? (
+                <NavDropdown
+                  title={"Hola " + user.name.split(" ")[0]}
+                  id="basic-nav-dropdown"
                 >
-                  Servicios
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <HashLink
-                  to="/#planes"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Planes
-                </HashLink>
-              </li>
-
-              <li className="nav-item">
-                <HashLink
-                  to="/#beneficios"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Beneficios
-                </HashLink>
-              </li>
-
-              <li className="nav-item">
-                <HashLink
-                  to="/#equipo"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Equipo
-                </HashLink>
-              </li>
-
-              {/* <li className='nav-item'>
-                <Link
-                  to='/create-student'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  CrearST
-                </Link>
-                </li>
-
-                <li className='nav-item'>
-                <Link
-                  to='/edit-student'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  EditarST
-                </Link>
-                </li>
-
-                <li className='nav-item'>
-                <Link
-                  to='/student-list'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  ListarST
-                </Link>
-                </li> 
-              */}
-
-              <li className="nav-item">
-                <HashLink
-                  to="/#footer"
-                  className="nav-links"
-                  onClick={closeMobileMenu}
-                >
-                  Encuéntranos
-                </HashLink>
-              </li>
-
-              {button ? (
-                auth.isAuthenticated ? (
+                  <LinkContainer to="/dashboard">
+                    <NavDropdown.Item>Panel</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item href="">Preferencias</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#login" onClick={() => logoutUser()}>
+                    SALIR
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <>
                   <li className="nav-btn">
-                    <Link to="/login" className="btn-link">
-                      <Button buttonStyle="btn--outline" onClick={()=>logoutUser()}>SALIR</Button>
+                    <Link to="/register" className="btn-link">
+                      <Button buttonStyle="btn--outline">REGISTRARSE</Button>
                     </Link>
                   </li>
-                ) : (
-                  <>
-                    <li className="nav-btn">
-                      <Link to="/register" className="btn-link">
-                        <Button buttonStyle="btn--outline">REGISTRARSE</Button>
-                      </Link>
-                    </li>
-                    <li className="nav-btn">
-                      <Link to="/login" className="btn-link">
-                        <Button buttonStyle="btn--outline">INGRESAR</Button>
-                      </Link>
-                    </li>
-                  </>
-                )
-              ) : auth.isAuthenticated ? (
-                <Link to="/login" className="btn-link">
-                  <Button
-                    buttonStyle="btn--outline"
-                    buttonSize="btn--mobile"
-                    onClick={closeMobileMenu}
-                  >
-                    SALIR
-                  </Button>
-                </Link>
-              ) : (
-                <Link to="/login" className="btn-link">
-                  <Button
-                    buttonStyle="btn--outline"
-                    buttonSize="btn--mobile"
-                    onClick={closeMobileMenu}
-                  >
-                    INGRESAR
-                  </Button>
-                </Link>
-              )}
-            </ul>
-          </div>
-        </nav>
-      </IconContext.Provider>
-    </>
+                  <li className="nav-btn">
+                    <Link to="/login" className="btn-link">
+                      <Button buttonStyle="btn--outline">INGRESAR</Button>
+                    </Link>
+                  </li>
+                </>
+              )
+            ) : auth.isAuthenticated ? (
+              <>
+                <li className="nav-btn">
+                  <Link to="/dashboard" className="btn-link">
+                    <Button
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--mobile"
+                      onClick={closeMobileMenu}
+                    >
+                      PANEL
+                    </Button>
+                  </Link>
+                </li>{" "}
+                <li className="nav-btn">
+                  <Link to="/login" className="btn-link">
+                    <Button
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--mobile"
+                      onClick={() => {
+                        logoutUser();
+                        closeMobileMenu();
+                      }}
+                    >
+                      SALIR
+                    </Button>
+                  </Link>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className="nav-btn">
+                  <Link to="/register" className="btn-link">
+                    <Button
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--mobile"
+                      onClick={closeMobileMenu}
+                    >
+                      REGISTRARSE
+                    </Button>
+                  </Link>
+                </li>
+                <li className="nav-btn">
+                  <Link to="/login" className="btn-link">
+                    <Button
+                      buttonStyle="btn--outline"
+                      buttonSize="btn--mobile"
+                      onClick={closeMobileMenu}
+                    >
+                      INGRESAR
+                    </Button>
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </IconContext.Provider>
   );
 };
 // export default Navbar;
@@ -195,4 +199,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
   errors: state.errors,
 });
-export default connect(mapStateToProps,{logoutUser})(Navbar);
+export default connect(mapStateToProps, { logoutUser })(Navbar);
